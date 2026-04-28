c42e98ef73dcf71fdfdc48b5d870a1e49ae0122f
Elias Tracy <elitracy19@gmail.com>
2026-04-27 22:41:33 -0500

    feat: added go debugger (dap)

---
+++ lua/elias/plugins/dap.lua
@@ -0,0 +1,51 @@
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "leoluz/nvim-dap-go",
+        "nvim-neotest/nvim-nio",
+        "rcarriga/nvim-dap-ui"
+    },
+    config = function()
+        local dap, dapui = require('dap'), require('dapui')
+        local dapgo = require('dap-go')
+        dapui.setup()
+        dapgo.setup()
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+
+
+        -- Include the next few lines until the comment only if you feel you need it
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+        -- Include everything after this
+
+
+        vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end)
+        vim.keymap.set('n', '<leader>ds', function() require('dap').step_over() end)
+        vim.keymap.set('n', '<leader>di', function() require('dap').step_into() end)
+        vim.keymap.set('n', '<leader>do', function() require('dap').step_out() end)
+        vim.keymap.set('n', '<Leader>db', function()
+            require('dap').toggle_breakpoint()
+        end)
+        vim.keymap.set('n', '<Leader>dB', function()
+            require('dap').set_breakpoint()
+        end)
+        vim.keymap.set('n', '<Leader>dp', function()
+            require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+        end)
+        vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+        vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+
+        vim.keymap.set('n', '<Leader>dw', function() dapui.open() end)
+        vim.keymap.set('n', '<Leader>dW', function() dapui.close() end)
+    end
+
+}
