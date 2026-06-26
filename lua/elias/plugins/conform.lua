e9ce4dae255b7053e096efccf82351f12b1874cd
Elias Tracy <elitracy19@gmail.com>
2026-06-26 12:51:15 -0500

    feat: maps and python

---
+++ lua/elias/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                python = { "black" },
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+        })
+    end,
+}
